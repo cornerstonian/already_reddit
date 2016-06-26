c1b76ef48220542822a75feabca656842e43d1d4
cornerstonian <vois.cornerstone@gmail.com>
2016-06-25 21:48:35 -0500

    initial commit, boostrap added, post controller and model generated

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Post.create(title: 'Ruby Explained', url: 'http://buff.ly/28Xu8Jb')
+Post.create(title: 'Stack Overflow', url: 'http://stackoverflow.com')
+Post.create(title: 'Stop Watching F*****g Lost', url: 'https://www.youtube.com/watch?v=NK3sEX5Z35M')
+Post.create(title: 'What is An Object (Java)', url: 'http://buff.ly/28ZXzM6')
+Post.create(title: 'Rails: Difference Btwn a Layout & A View', url: 'http://buff.ly/28Xv6Fv')
+Post.create(title: 'The Batcave You Always Wanted', url: 'hhttp://growtheheckup.com/get-that-batcave-inspired-secret-passage-youve-always-wanted')
+Post.create(title: 'Why School Sucks', url: 'https://www.youtube.com/watch?v=MDreKHwrA-k')
+Post.create(title: ' The Iron Yard', url: 'https://www.theironyard.com')
+Post.create(title: 'Rails Params Hash Explained', url: 'https://www.youtube.com/watch?v=y57OnWV6dRE')
+Post.create(title: 'Routing In Rails', url: 'http://www.theodinproject.com/ruby-on-rails/routing')
